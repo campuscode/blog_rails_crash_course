5ea433df03fc172b6d7180ef09add37b2210035b
Alan Rafael R. Batista <alanrrb@gmail.com>
2015-09-05 01:37:07 -0300

    Add comments to post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
 
-    if post.comments.create(comment_params)
+    if @post.comments.create(comment_params)
       redirect_to @post, notice: 'Comment was successfully created'
     else
       redirect_to @post, alert: 'Error creating comment'
